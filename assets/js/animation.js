b4a445a4a54b2e3ca0c1d401524d4766dacf59cf
victorcdrdm <victorcrombez@gmail.com>
2021-04-15 16:02:26 +0200

    fix css

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -13,24 +13,13 @@ technologies.forEach(technology => {
             return;
         } else {
             technology.classList.toggle('active');
-
         }
         index = technology.getAttribute('data-show');
 
-        for (i = 0; i < technologies.length; i++) {
-            if (technologies[i].getAttribute('data-show') !== index) {
-                technologies[i].classList.remove('active');
-            }
-        }
-        for (j = 0; j < projects.length; j++) {
-            if (projects[j].getAttribute('data-show') === index) {
-                projects[j].classList.toggle('active-block')
-            } else {
-                projects[j].classList.remove('active-block')
-            }
-        }
+        showProjects(index)
     })
 })
+
 if (selectMethodology){
     selectMethodology.addEventListener('click', () => {
         selectMethodology.classList.add('active')
@@ -49,10 +38,56 @@ if (selectMethodology) {
     })
 }
 
+//slider-technologies//
+
+    const slides = document.getElementsByClassName("technology-slide")
+    const blockTechnologies = document.getElementById('slides')
+
+
+    document.getElementById("btnback").addEventListener("click",() => {
+        futureSlide(-1)
+    })
+    document.getElementById("btnnext").addEventListener("click",() => {
+        futureSlide(1)
+    })
+    let slideindex = 1
+    newSlide(slideindex)
+    function futureSlide(n) {
+        newSlide(slideindex += n)
+    }
+    function newSlide(n) {
+        if (n < 1) {
+            slideindex = 1
+
+        }
+        if (n > slides.length) {
+            slideindex = slides.length
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+            slides[i].classList.remove('active')
+        }
+        slides[slideindex-1].classList.add('active')
+        slides[slideindex-1].style.display = "block"
+        let index = slides[slideindex-1].getAttribute('data-show')
+        showProjects(index)
+    }
 
 
 
 
-
-
+function showProjects(index){
+    for (i = 0; i < technologies.length; i++) {
+        if (technologies[i].getAttribute('data-show') !== index) {
+            technologies[i].classList.remove('active');
+        }
+    }
+    for (j = 0; j < projects.length; j++) {
+        if (projects[j].getAttribute('data-show') === index) {
+            projects[j].classList.toggle('active-block')
+        } else {
+            projects[j].classList.remove('active-block')
+        }
+    }
+}
 
